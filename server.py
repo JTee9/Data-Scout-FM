6d06aa42d959bcf003f2c75d0df9397afaa73735
Jimmy T <jimmytalbot9@gmail.com>
2025-03-08 12:09:08 +0900

    Add host='0.0.0.0.' to app.run

--- server.py
+++ server.py
@@ -194,7 +194,7 @@ def toggle_modal(open_clicks, close_clicks, is_open):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
 
 
 ## Website details
